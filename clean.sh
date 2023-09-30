1c4eafa0e8decc63645381a2bc4a6acbd78c9da8
Jiri Benes <benes.jiri@shoppingarena.eu>
2023-10-01 02:18:00 +0300

    edit clean index.js App.js

--- clean.sh
+++ clean.sh
@@ -5,3 +5,19 @@
 # It will delete files and lines
 # delete line3 src/index.js
 sed '3d' -i src/index.js
+
+# Specify the file path
+file="src/App.js"
+# delete lines from App.js
+sed '1,2d' -i "$file"
+
+
+
+# Get the total line count of the file
+# total_lines=$(wc -l < "$file")
+
+# Calculate the adjusted line positions
+start_line=$((6 - 2))
+end_line=$((21 - 2))
+# Delete lines 6 to 21
+sed -i "${start_line},${end_line}d" "$file"
